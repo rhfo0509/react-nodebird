c849136c8064dcffc31808d186c9c0b41c62cc89
bear <ungnam0509@gmail.com>
2023-05-24 01:36:45 +0900

    Add: get (un)related posts

--- ch5/back/routes/posts.js
+++ ch5/back/routes/posts.js
@@ -42,4 +42,90 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/related", async (req, res, next) => {
+  try {
+    const me = await User.findByPk(req.user.id);
+    const followings = await me.getFollowings();
+
+    const posts = await Post.findAll({
+      order: [
+        ["createdAt", "DESC"],
+        [Comment, "createdAt", "DESC"],
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+          where: {
+            id: {
+              [Op.in]: followings.map((v) => v.id),
+            },
+          },
+        },
+        {
+          model: Comment,
+          include: [{ model: User, attributes: ["id", "nickname"] }],
+        },
+        { model: Image },
+        { model: User, attributes: ["id"], as: "Likers" },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            { model: User, attributes: ["id", "nickname"] },
+            { model: Image },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/unrelated", async (req, res, next) => {
+  try {
+    const me = await User.findByPk(req.user.id);
+    const followings = await me.getFollowings();
+
+    const posts = await Post.findAll({
+      order: [
+        ["createdAt", "DESC"],
+        [Comment, "createdAt", "DESC"],
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+          where: {
+            id: {
+              [Op.notIn]: followings.map((v) => v.id).concat(req.user.id),
+            },
+          },
+        },
+        {
+          model: Comment,
+          include: [{ model: User, attributes: ["id", "nickname"] }],
+        },
+        { model: Image },
+        { model: User, attributes: ["id"], as: "Likers" },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            { model: User, attributes: ["id", "nickname"] },
+            { model: Image },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;
